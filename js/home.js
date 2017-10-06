dfb144224ce0b30d80626f0eacc3496796925574
Winston Parker <wmparker@mtu.edu>
2017-10-06 17:50:48 -0400

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,36 @@
+
+
+$(document).ready(function() {
+    $('#trips').DataTable({
+      "searching": true,
+      "filter": true,
+      "oLanguage": { "sSearch": "" },
+      "lengthChange": false,
+      "bInfo" : true,
+      initComplete: function () {
+        $("#trips_filter").detach().appendTo('#search');
+          $("#trips_filter").find('input').attr("placeholder", "Search Trips");
+          
+          $("#trips_filter").find('input').on("focus keyup", function(e) {
+    
+            $('html, body').animate({
+                scrollTop: $("#trips").offset().top
+            }, 1000);
+          });
+           
+          
+      }
+    });
+    
+    
+    checkWidth();
+} );
+
+
+function checkWidth() {
+var a=$('.cover-img').height();
+$('.cover').height(a);
+}
+
+// Bind event listener
+$(window).resize(checkWidth);
