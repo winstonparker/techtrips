d81a0eea6b3992e92a4ef747ea0804d70bc9343d
winstonparker <winstonparker@Winstons-MBP.attlocal.net>
2017-10-15 14:23:03 -0400

    added maps

---
+++ js/plan.js
@@ -0,0 +1,14 @@
+
+//setup date fields
+$( "#start-date" ).datepicker();
+$( "#end-date" ).datepicker();
+
+// //setup time fields
+$('#start-time').timepicker({ 
+    'scrollDefault': 'now',
+    'disableTextInput': true    
+ });
+$('#end-time').timepicker({ 
+    'scrollDefault': 'now',
+    'disableTextInput': true    
+});
