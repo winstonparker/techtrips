a7d51fc5eef9b98d7a51acde40f51cac28812960
24g <24g@24gs-MacBook-Pro-2.local>
2017-10-16 17:32:05 -0400

    post:

--- js/plan.js
+++ js/plan.js
@@ -12,3 +12,30 @@ $('#end-time').timepicker({
     'scrollDefault': 'now',
     'disableTextInput': true    
 });
+
+
+//book trip
+$('#book').on("click touch", function(){
+
+    $.ajax({
+        url: 'php/book.php',
+        type: 'POST',
+        dataType: "text",
+        data: { 
+            'trip_name': $("#trip-name").val(), 
+            'location_name': $("#location-name").val(),
+            'address': $("#address").val(),
+            'meeting_location': $("#meeting-location").val(),
+            'exta': "hello"
+            
+        } ,
+        success: function(data, status) {
+            console.log(data);
+           window.location.replace("php/book.php")
+        }
+    });
+
+
+
+
+});
---
+++ php/book.php
@@ -0,0 +1,13 @@
+<?php 
+
+var_dump($_POST);
+
+$tripName = $_POST["trip_name"];
+$locationName = $_POST["location_name"];
+$address = $_POST["address"];
+$meetingLocation = $_POST["meeting_location"];
+$startDate = $_POST["start_date"];
+$endDate = $_POST["end_date"];
+
+
+
